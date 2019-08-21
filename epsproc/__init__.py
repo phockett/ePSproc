01aaabe301af11d96e537e41a82f933dcb9bae7f
phockett <phockett@users.noreply.github.com>
2019-08-21 12:29:44 -0400

    Update __init__.py

--- epsproc/__init__.py
+++ epsproc/__init__.py
@@ -1,1 +1,18 @@
 __version__ = '1.1.0'
+
+# Import functions
+# from epsproc import ePSproc_IO as epIO
+# from epsproc import ePSproc_util as epUT
+# from epsproc import ePSproc_sphCalc as epSC
+# from epsproc import ePSproc_sphPlot as epSP
+# from epsproc import ePSproc_MFPAD as epMF
+
+import epsproc.ePSproc_IO as IO
+import epsproc.ePSproc_util as UT
+import epsproc.ePSproc_sphCalc as SC
+import epsproc.ePSproc_sphPlot as SP
+import epsproc.ePSproc_MFPAD as MF
+
+
+# Import shared modules
+# TODO - move from individual modules.

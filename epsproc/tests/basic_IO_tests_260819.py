8edc55cd8ee3d1f9a397a97e2bf820614cb9e2c1
phockett <phockett@gmail.com>
2019-08-26 17:45:51 -0400

    Update basic_IO_tests_260819.py

--- epsproc/tests/basic_IO_tests_260819.py
+++ epsproc/tests/basic_IO_tests_260819.py
@@ -40,7 +40,7 @@ fList = ep.getFiles(fileBase = dataPath)
 
 #%% Read file
 
-file = fList[0]
+file = fList[0]  # HERE SET FOR N2 multi-E data
 lines, dumpSegs = ep.dumpIdyFileParse(file)
 
 #%% Check parameters
@@ -63,3 +63,31 @@ for dumpSeg in dumpSegs:
 #%% Parse matrix elements - Xarray format (already includes loop over segments)
 
 data, blankSegs = ep.dumpIdySegsParseX(dumpSegs, ekeList, symSegs)
+
+#%% Full file IO
+
+dataSet = ep.readMatEle(fileIn = file)
+
+#%% Test sph multiplication over all dims
+
+res = 50
+daRed = ep.matEleSelector(dataSet[0], thres = 1e-2, inds = {'Type':'L','it':1})
+
+# Generate spherical harmonics
+Lmax = daRed.l.max()
+YlmX = ep.sphCalc(Lmax, res = res)
+
+# Multiply
+test = daRed * YlmX
+
+# Check some terms by selecting a single set of matrix elements...
+inds = {'mu':0, 'Eke':0.1, 'Cont':'SU'}
+daRed.sel(inds)
+
+# Values should match file:
+#   m   l mu ip it Value
+#   0   1  0  1  1  0.27363206E+01 -0.92696864E-01
+#   0   3  0  1  1 -0.17158793E+00 -0.79529163E+00
+
+test.sel(inds)
+ep.sphSumPlotX(test.sel(inds).squeeze(dim = 'Sym'))

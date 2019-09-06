9e16788f59f2bdc4522d58c4d01716b75a2ca8c4
phockett <phockett@users.noreply.github.com>
2019-09-06 12:46:17 -0400

    RTD Sphinx fix.

--- docs/doc-source/conf.py
+++ docs/doc-source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..\\'))    # Add module path (relative to docs path)
 sys.path.insert(0, os.path.abspath('..\\..\\'))    # Add module path (relative to docs path)
 # print(sys.path)
 

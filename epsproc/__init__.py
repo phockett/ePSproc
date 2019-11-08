3d48d89b9db1f83f52365cb138b08e33a1d121f4
phockett <phockett@users.noreply.github.com>
2019-11-08 13:49:51 -0500

    Version incremented ready for merge.

--- epsproc/__init__.py
+++ epsproc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.0 dev'
+__version__ = '1.2.0'
 
 # Import shared packages - actually, should be at module (file) level?
 # https://stackoverflow.com/questions/8165703/python-imports-across-modules-and-global-variables

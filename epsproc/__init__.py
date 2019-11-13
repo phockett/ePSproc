2ad9f01d85d20effab260296e2d1ab55fb280fd5
phockett <phockett@users.noreply.github.com>
2019-11-12 20:27:08 -0500

    Version patch for release v1.2.1.
    Fixes (some) write netCDF issues.

--- epsproc/__init__.py
+++ epsproc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 # Import shared packages - actually, should be at module (file) level?
 # https://stackoverflow.com/questions/8165703/python-imports-across-modules-and-global-variables

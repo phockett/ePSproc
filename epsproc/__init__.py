f084fde91d90ec62c92ac914e28c89ac68dedf40
phockett <phockett@users.noreply.github.com>
2020-06-19 09:00:13 -0400

    Init/mkinit debug.

--- epsproc/__init__.py
+++ epsproc/__init__.py
@@ -1,3 +1,52 @@
+__version__ = '1.2.5-dev'
+
+# Import shared packages - actually, should be at module (file) level?
+# https://stackoverflow.com/questions/8165703/python-imports-across-modules-and-global-variables
+# OR GLOBAL...?
+# https://stackoverflow.com/questions/11990556/how-to-make-global-imports-from-a-function
+#def my_imports(module_name):
+#    globals()[module_name] = __import__(module_name)
+
+# import numpy as np
+
+
+# Import functions
+#from epsproc import ePSproc_IO as epIO
+#from epsproc import ePSproc_util as epUT
+#from epsproc import ePSproc_sphCalc as epSC
+#from epsproc import ePSproc_sphPlot as epSP
+#from epsproc import ePSproc_MFPAD as epMF
+
+#import epsproc.ePSproc_IO as IO
+#import epsproc.ePSproc_util as UT
+#import epsproc.ePSproc_sphCalc as SC
+#import epsproc.ePSproc_sphPlot as SP
+#import epsproc.ePSproc_MFPAD as MF
+
+# Direct * import for modules - probably a bad idea.
+# from epsproc.IO import *
+# from epsproc.util import *
+# from epsproc.basicPlotters import *
+# from epsproc.sphCalc import *
+# from epsproc.sphPlot import *
+# from epsproc.MFPAD import *
+# from epsproc.MFBLM import *
+# from epsproc.AFBLM import afblm, AFBLMCalcLoop
+# from epsproc.conversion import multiDimXrToPD
+
+
+
+# TODO - move from individual modules.
+# from epsproc.util import matEleSelector
+
+# Testing mkinit, https://github.com/Erotemic/mkinit
+# On Stimpy, run with
+# > mkinit D:\code\github\ePSproc\epsproc > mkinitOut.txt
+# Writing directly to __init__.py sets autogen code only?
+
+# Autogen with mkinit, https://github.com/Erotemic/mkinit
+# <AUTOGEN_INIT>
+
 from epsproc import AFBLM
 from epsproc import IO
 from epsproc import MFBLM
@@ -75,3 +124,4 @@ __all__ = ['ADMdimList', 'AFBLM', 'AFBLMCalcLoop', 'Arrow3D', 'BLMdimList',
            'w3jVecMethods', 'w3jguVecCPU', 'w3jprange', 'wDcalc',
            'writeOrb3Dvtk', 'writeXarray']
 
+# </AUTOGEN_INIT>

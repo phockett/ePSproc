ebdfe705b78678976e3936a05a33b5815765b84f
phockett <phockett@users.noreply.github.com>
2020-09-27 16:25:25 -0400

    Added basic lmPlot() multijob method, and general debug.

--- epsproc/classes/multiJob.py
+++ epsproc/classes/multiJob.py
@@ -25,6 +25,39 @@ class ePSmultiJob():
     - Sort to dictionaries and Xarray datasets (needs some work).
     - Comparitive plotting (in development).
 
+
+    Parameters
+    ----------
+    fileBase : str or Path object, default = None
+        Base directory to scan for ePS files, subdirs will also be searched.
+        This is required unless jobDirs is set instead.
+
+    jobDirs : list of str or Path objects, default= None
+        List of dirs containing ePS output files to read, subdirs will NOT be searched.
+
+    jobStructure : str, optional, default = None
+        This will be set automatically by self.scanDirs(), but can also be passed to override.
+        Values "subDirs" or "rootDir", in former case multiple files will be stacked by Eke.
+
+    prefix : str, optional, default = None
+        Set prefix string for file checks (cf. wfPlot class). NOT YET USED.
+
+    ext : str, optional, default = '.out'
+        Set default file extension for dir scanning.
+        This should match the file extension for ePolyScat output files.
+
+    Edp : int, optional, default = 2
+        Set default dp for Ehv conversion. May want to set this elsewhere instead... maybe just for plotting?
+        TODO: also consider axis reindex, lookups and interp functions here - useful for differences between datasets.
+
+    verbose : int, optional, default = 1
+        Set verbosity level for printing/error checking.
+        Not yet fully implemented, but, generally:
+        - 0, no printed output.
+        - 1, basic printed info.
+        - 2, print all info, including subfunction outputs.
+
+
     TODO:
 
     - verbosity levels, subtract for subfunctions? Or use a dict to handle multiple levels?
@@ -35,9 +68,11 @@ class ePSmultiJob():
 
     # TODO: set to varg in for jobs dict
     def __init__(self, fileBase = None, jobDirs = None, jobStructure = None,
-                 prefix = None, ext = '.out', verbose = 1):
+                 prefix = None, ext = '.out', Edp = 2, verbose = 1):
 
         self.verbose = verbose
+
+        self.Edp = Edp  # Set default dp for Ehv conversion. May want to set this elsewhere...?
 
         # Set file properties
         self.jobs = {'fileBase':Path(fileBase),
@@ -62,6 +97,7 @@ class ePSmultiJob():
         - Old structure, with multiple job output files per dir (but not per E range).
         - New structure, with multiple output files per E range, subdirs per job/orb.
 
+        This is set in jobStructure variable, as "rootDir" or "subDirs" respectively.
 
         """
 
@@ -70,7 +106,7 @@ class ePSmultiJob():
         # Subdirs only - NOT ROOT!
         testSub = [p for p in self.jobs['fileBase'].iterdir() if (p.is_dir() and bool(list(p.glob(f"*{self.jobs['ext']}"))))]
 
-        if testSub and (self.jobs['jobStructure'] is None or 'subDir'):
+        if testSub and (self.jobs['jobStructure'] is None or 'subDirs'):
             self.jobs['jobDirs'] = testSub
             self.jobs['jobStructure'] = 'subDirs'
 
@@ -146,8 +182,8 @@ class ePSmultiJob():
                 dataXS[0].attrs['orbX'], dataXS[0].attrs['orbInfo'] = getOrbInfo(dataXS[0].attrs['jobInfo'], dataXS[0].attrs['molInfo'])
 
                 # Set absolute photon energy
-                dataXS[0]['Ehv'] = dataXS[0]['Ehv'] - (float(dataXS[0].jobInfo['IPot']) + dataXS[0].orbX['E'].data[0])
-                dataMatE[0]['Ehv'] = dataXS[0]['Ehv'] - (float(dataXS[0].jobInfo['IPot']) + dataXS[0].orbX['E'].data[0])
+                dataXS[0]['Ehv'] = (dataXS[0]['Ehv'] - (float(dataXS[0].jobInfo['IPot']) + dataXS[0].orbX['E'].data[0])).round(self.Edp)
+                dataMatE[0]['Ehv'] = (dataXS[0]['Ehv'] - (float(dataXS[0].jobInfo['IPot']) + dataXS[0].orbX['E'].data[0])).round(self.Edp)
 
                 # Set as xr.ds(), label by orbital
                 # TODO: fix this, in some cases gives errors with multiple values - probably an Ehv issue?
@@ -165,7 +201,7 @@ class ePSmultiJob():
                 jobNotes = []
                 jobNotes.append({ 'batch': dataXS[0].jobInfo['comments'][0].strip('#').strip(),
                                 'event': dataXS[0].jobInfo['comments'][1].split(',', maxsplit=1)[1].strip(),
-                                'orbLabel': dataXS[0].jobInfo['comments'][1].split('(', maxsplit=1)[1].strip(').'),
+                                'orbLabel': dataXS[0].jobInfo['comments'][1].split('(', maxsplit=1)[1].split(')')[0],
                                 'orbE': dataXS[0].orbX['E'].data[0]
                                 })
 
@@ -188,8 +224,8 @@ class ePSmultiJob():
                     dataXS[m].attrs['orbX'], dataXS[m].attrs['orbInfo'] = getOrbInfo(dataXS[m].attrs['jobInfo'], dataXS[m].attrs['molInfo'])
 
                     # Set absolute photon energy
-                    dataXS[m]['Ehv'] = dataXS[m]['Ehv'] - (float(dataXS[m].jobInfo['IPot']) + dataXS[m].orbX['E'].data[0])
-                    dataMatE[m]['Ehv'] = dataXS[m]['Ehv'] - (float(dataXS[m].jobInfo['IPot']) + dataXS[m].orbX['E'].data[0])
+                    dataXS[m]['Ehv'] = (dataXS[m]['Ehv'] - (float(dataXS[m].jobInfo['IPot']) + dataXS[m].orbX['E'].data[0])).round(self.Edp)
+                    dataMatE[m]['Ehv'] = (dataXS[m]['Ehv'] - (float(dataXS[m].jobInfo['IPot']) + dataXS[m].orbX['E'].data[0])).round(self.Edp)
 
                     # Set as xr.ds(), label by orbital
                     dsXS[f"orb{dataXS[m].orbX['orb'].data[0]}"] = dataXS[m]
@@ -200,7 +236,7 @@ class ePSmultiJob():
                     # This is basically as per summary.jobInfo() routine
                     jobNotes.append({ 'batch': dataXS[m].jobInfo['comments'][0].strip('#').strip(),
                                     'event': dataXS[m].jobInfo['comments'][1].split(',', maxsplit=1)[1].strip(),
-                                    'orbLabel': dataXS[m].jobInfo['comments'][1].split('(', maxsplit=1)[1].strip(').'),
+                                    'orbLabel': dataXS[m].jobInfo['comments'][1].split('(', maxsplit=1)[1].split(')')[0],
                                     'orbE': dataXS[m].orbX['E'].data[0]
                                     })
 
@@ -279,6 +315,24 @@ class ePSmultiJob():
 
 
     def plotGetCro(self, pType = 'SIGMA', Erange = None, Etype = 'Eke'):
+        """
+        Basic GetCro (cross-section) data plotting for multijob class. Run self.plot.line(x=Etype, col='Type') for each dataset.
+        (See :py:func:`epsproc.classes.ePSmultiJob.plotGetCroComp()` for comparitive plots over datasets.)
+
+        Note this is for LF averaged parameters, for more details see the `ePS starter notes <https://epsproc.readthedocs.io/en/latest/ePS_ePSproc_tutorial/ePS_tutorial_080520.html#Results>`_ for more details.
+
+        Parameters
+        ----------
+        pType : str, optional, default = 'SIGMA'
+            Set data for plotting, either 'SIGMA' (cross-section) or 'BETA' (B2 parameter).
+
+        Erange : list of int or float, optional, default = None
+            Set plot range [Emin, Emax]. Defaults to full data range if not set
+
+        Etype : str, optional, default = 'Eke'
+            Set plot dimension, either 'Eke' (electron kinetic energy) or 'Ehv' (photon energy).
+
+        """
 
 #         if self.jobs['jobStructure'] == 'subDirs':
         for key in self.dataSets:
@@ -295,21 +349,53 @@ class ePSmultiJob():
                 # Subset before plot to avoid errors on empty array selection!
                     subset = self.dataSets[key]['XS'][m].swap_dims({'Eke':'Ehv'}).sel(XC=pType, **{Etype:slice(Erange[0], Erange[1])})   # With dict unpacking for var as keyword
 
-                    if subset.any():
-                        subset.plot.line(x=Etype, col='Type')
+                    # if subset.any():
+                    #     subset.plot.line(x=Etype, col='Type')
 
                 else:
                     subset = self.dataSets[key]['XS'][m].sel(XC=pType, **{Etype:slice(Erange[0], Erange[1])})   # With dict unpacking for var as keyword
 
-                    if subset.any():
-                        subset.plot.line(x=Etype, col='Type')
+                if subset.any():
+                    subset.plot.line(x=Etype, col='Type')
 
-
+                if pType == 'SIGMA':
+                    plt.ylabel('XS/Mb')
+                else:
+                    plt.ylabel(r"$\beta_{LM}$")
 #         else:
 
     # Basically as per plotGetCro, but subselect and put on single plot.
     # Should do all of this with Holoviews...!
     def plotGetCroComp(self, pType='SIGMA', pGauge='L', pSym=('All','All'), Erange = None, Etype = 'Eke'):
+        """
+        Basic GetCro (cross-section) data plotting for multijob class, comparitive plots.
+        Run self.plot.line(x=Etype) for each dataset after subselection on Gauge and Symmetry, and use single axis.
+        (See :py:func:`epsproc.classes.ePSmultiJob.plotGetCro()` for plots per dataset.)
+
+        Note this is for LF averaged parameters, for more details see the `ePS starter notes <https://epsproc.readthedocs.io/en/latest/ePS_ePSproc_tutorial/ePS_tutorial_080520.html#Results>`_ for more details.
+
+        Parameters
+        ----------
+        pType : str, optional, default = 'SIGMA'
+            Set data for plotting, either 'SIGMA' (cross-section) or 'BETA' (B2 parameter).
+
+        pGauge : str, optional, default = 'L'
+            Set gauge, either 'L' (Length), 'V' (Velocity) or 'M' (Mixed)
+
+        pSym : tuple of strs, optional, default = ('All','All')
+            Select symmetry, (Cont, Targ).
+            Default value will plot all allowed symmetries.
+
+        Erange : list of int or float, optional, default = None
+            Set plot range [Emin, Emax]. Defaults to full data range if not set
+
+        Etype : str, optional, default = 'Eke'
+            Set plot dimension, either 'Eke' (electron kinetic energy) or 'Ehv' (photon energy).
+
+        """
+
+
+
         # Comparison plots over orbs
 
         # from matplotlib import pyplot as plt  # For legend
@@ -330,14 +416,14 @@ class ePSmultiJob():
                     # Subset before plot to avoid errors on empty array selection!
                     subset = self.dataSets[key]['XS'][m].swap_dims({'Eke':'Ehv'}).sel(XC=pType, Type=pGauge, Sym=pSym, **{Etype:slice(Erange[0], Erange[1])})  # With dict unpacking for var as keyword
 
-                    if subset.any():
-                        pltObj = subset.plot.line(x=Etype)
+                    # if subset.any():
+                    #     pltObj = subset.plot.line(x=Etype)
 
                 else:
                     subset = self.dataSets[key]['XS'][m].sel(XC=pType, Type=pGauge, Sym=pSym, **{Etype:slice(Erange[0], Erange[1])})   # With dict unpacking for var as keyword
 
-                    if subset.any():
-                        pltObj = subset.plot.line(x=Etype)
+                if subset.any():
+                    pltObj = subset.plot.line(x=Etype)
 
                 # Label with orb_sym
 #                 lText.append(self.dataSets[key]['XS'][m].attrs['fileBase'].rsplit('/',maxsplit=1)[0])
@@ -352,3 +438,70 @@ class ePSmultiJob():
             plt.ylabel('XS/Mb')
         else:
             plt.ylabel(r"$\beta_{LM}$")
+
+
+    def lmPlot(self, Erange = None, Etype = 'Eke', refData = None, **kwargs):
+        """
+        Wrapper for :py:func:`epsproc.lmPlot` for multijob class. Run lmPlot() for each dataset.
+
+        Parameters
+        ----------
+        Erange : list of int or float, optional, default = None
+            Set plot range [Emin, Emax]. Defaults to full data range if not set
+
+        Etype : str, optional, default = 'Eke'
+            Set plot dimension, either 'Eke' (electron kinetic energy) or 'Ehv' (photon energy).
+
+        refData : tuple (key,m), optional, default = None
+            If set, calculate difference plots against reference dataset.
+            TODO: implement difference plots.
+            TODO: implement testing logic, may fail without E-axis forcing, and sym summation?
+
+        kwargs : dict, optional, default = {}
+            Plotting options to pass to :py:func:`epsproc.lmPlot`.
+            These will also be set in self.lmPlotOpts for further use.
+            Note that any existing options in self.lmPlotOpts will also be used, or overwritten if matching keys are found.
+
+
+        Notes
+        -----
+        Basic scheme from ePSmultijob.plotGetCro, which loops and switches on Eke/Ehv. Should tidy up at some point.
+
+        """
+
+        # Set lmPlotOpts
+        # Check passed args vs. self.lmPlotOpts and overwrite
+        if kwargs:
+            for key, value in kwargs.items():
+                self.lmPlotOpts[key] = value
+
+
+        # Loop over datasets
+        for key in self.dataSets:
+    #                 testClass.dataSets[key]['XS'][0].sel(XC='SIGMA', Eke=slice(Erange[0], Erange[1])).plot.line(x='Eke', col='Type')   # This works
+
+            # Init empty list for daPlotpd data
+            self.dataSets[key]['daPlotpd'] = []
+
+            for m, item in enumerate(self.dataSets[key]['matE']):
+
+                # Set default to full range, same for all cases
+                if Erange is None:
+                    Erange = [self.dataSets[key]['matE'][m][Etype].min().data, self.dataSets[key]['matE'][m][Etype].max().data]
+
+
+                # More elegant way to swap on dims?
+                if Etype == 'Ehv':
+                # Subset before plot to avoid errors on empty array selection!
+                    subset = self.dataSets[key]['matE'][m].swap_dims({'Eke':'Ehv'}).sel(**{Etype:slice(Erange[0], Erange[1])})   # With dict unpacking for var as keyword
+
+                else:
+                    subset = self.dataSets[key]['matE'][m].sel(**{Etype:slice(Erange[0], Erange[1])})   # With dict unpacking for var as keyword
+
+                # Run lmPlot
+                if subset.any():
+                    daPlot, daPlotpd, legendList, gFig = ep.lmPlot(subset, xDim = Etype, **self.lmPlotOpts)
+                else:
+                    daPlotpd = None
+
+                self.dataSets[key]['daPlotpd'].append(daPlotpd)  # Set to include None cases to keep indexing. Should set as dict instead?

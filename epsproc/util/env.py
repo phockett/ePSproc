b437112e648589818a0ec100ca9be8649a84d64b
phockett <phockett@users.noreply.github.com>
2021-08-16 11:20:02 -0400

    Added warnings control for notebook env.

--- epsproc/util/env.py
+++ epsproc/util/env.py
@@ -5,6 +5,8 @@ ePSproc util functions for environment checks and related.
 
 """
 
+# To control display of warnings...
+import warnings
 
 
 # Check if iPython/Notebook
@@ -22,22 +24,29 @@ ePSproc util functions for environment checks and related.
 
 # THIS works, IPython v7.1.3, Jupyter notebook 6.0.3
 # COULD also use Scooby here, see https://pypi.org/project/scooby/
-def isnotebook():
+def isnotebook(warn = 'once'):
     """
     Check if code is running in Jupyter Notebook.
 
     Taken verbatim from https://exceptionshub.com/how-can-i-check-if-code-is-executed-in-the-ipython-notebook.html
     Might be a better/more robust way to do this?
 
+    Also added optional "warn = 'once'", set 'once' or 'ignore' to control warnings in notebook env., see https://stackoverflow.com/a/9031848
+
     """
 
     try:
         shell = get_ipython().__class__.__name__
+
         if shell == 'ZMQInteractiveShell':
+            warnings.filterwarnings(warn)
             return True   # Jupyter notebook or qtconsole
+
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
+
         else:
             return False  # Other type (?)
+
     except NameError:
         return False      # Probably standard Python interpreter

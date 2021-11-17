00b54279a3cac26b704f0d2b72bd86e0af4132e0
phockett <phockett@users.noreply.github.com>
2021-11-17 12:31:02 -0500

    Force refDims to list if singleton.

--- epsproc/util/misc.py
+++ epsproc/util/misc.py
@@ -300,5 +300,8 @@ def subselectDims(data, refDims = []):
         return {k:v for k,v in refDims.items() if k in dimSets['shared']}
 
     else:
+        if not isinstance(refDims, list):
+            refDims = [refDims]  # Force to list.
+
         # return dimSets['shared']  # Return shared dim list only.
         return [item for item in refDims if item in dimSets['shared']]   # Return shared items with forced ordering to match inputs.

f14a7d4fdbfae89788e673c774d5c3db93c8095a
phockett <phockett@users.noreply.github.com>
2023-05-01 15:08:45 -0400

    Added getFilesFromGithub() and build_url() functions.

--- epsproc/util/io.py
+++ epsproc/util/io.py
@@ -3,7 +3,7 @@ ePSproc IO util functions
 
 Various tools for use in file IO.
 
-01/05/23    Added getFilesFromURLs.
+01/05/23    Added getFilesFromURLs, getFilesFromGithub and build_url functions.
             See also epsdata module for Zenodo-specific downloader.
 
 27/06/22    Split out from core IO.py, to extend backend options and support.
@@ -14,6 +14,7 @@ Various tools for use in file IO.
 from datetime import datetime as dt # Import datetime.datetime for now() function
 import wget
 from pathlib import Path
+import urllib
 
 from epsproc.util.epsdata import convert_bytes
 
@@ -156,9 +157,82 @@ def getFilesFromURLs(urls, dataName=None, dataPath=None, ghRaw = True):
             fDict[k] = {'url':v, 'localFile':Path(fout), 'downloaded':True}
 
         # Add file info
+        fDict[k]['name'] = fDict[k]['localFile'].name
         fDict[k]['filestat'] = fDict[k]['localFile'].lstat()
         fDict[k]['size'] = convert_bytes(fDict[k]['filestat'].st_size)
         fDict[k]['timestamp'] = dt.fromtimestamp(fDict[k]['filestat'].st_ctime).strftime('%H:%M:%S %d/%m/%Y')
 
 
     return fList, fDict
+
+
+def build_url(base_url, *paths, **params):
+    """
+    Build URL, adapted from https://stackoverflow.com/a/44552191 and https://stackoverflow.com/a/43934565
+    Thanks to [Michael Jaison G](https://stackoverflow.com/users/3152600/michael-jaison-g) and [Mike K](https://stackoverflow.com/users/1439255/mike-k)
+
+    url is built as (base_url + /all/path/elements + ?key=value) where key=value are kwargs in **params.
+
+    """
+
+    # Returns a list in the structure of urlparse.ParseResult
+    # url_parts = list(urllib.parse.urlparse(base_url))
+    # # url_parts.path = url_parts.path + '/'.join(paths)   # Add existing, but can't set like this!
+    # url_parts[2] = url_parts[2] + '/'.join(paths)
+    # url_parts[4] = urllib.parse.urlencode(params)
+    # print(url_parts)
+    # return urllib.parse.urlunparse(url_parts)
+
+    # Version with native object
+    # In this case urllib.parse.urlunparse(url) splits params with ';' instead of '?'? Can fix with .replace(';','?'), but seems odd.
+    url = urllib.parse.urlparse(base_url)
+    url = url._replace(path=url.path + '/'.join(paths))  # Keep existing path if present
+    url = url._replace(params=urllib.parse.urlencode(params))
+    return urllib.parse.urlunparse(url).replace(';','?')
+
+
+
+def getFilesFromGithub(subpath=[], owner='phockett', repo='epsproc', ref=None,
+                       download=True, dataName=None, dataPath=None, ghRaw = True,
+                       **params):
+    """
+    Grab repo file list from Github using rest API, format: `https://api.github.com/repos/:owner/:repo/contents/:subpath?ref=ref`
+
+    Where subpath and ref are optional (latter is tag or branch).
+
+    Use keyword args to pass any additional key=value args to URL builder.
+
+    See https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#get-repository-content
+
+    If download=True, pass outputs to getFilesFromURLs() function, with additional args dataName=None, dataPath=None.
+
+    """
+
+    # Configure full path
+    # url = 'https://api.github.com/repos/' + owner
+    # if ref is not None:
+
+    if ref is not None:
+        params.update({'ref':ref})
+
+    if isinstance(subpath,list):
+        url = build_url('https://api.github.com/repos/', owner, repo, 'contents', *subpath, **params)
+
+    else:
+        url = build_url('https://api.github.com/repos/', owner, repo, 'contents', subpath, **params)
+
+    # print(urllib.parse.urlunparse(url))
+    # return url
+    print(f"Querying URL: {url}")
+
+    fileDict = requests.get(url).json()
+
+    fileDictDownloads = {n:item['download_url'] for n,item in enumerate(fileDict)}
+
+    if download:
+        # Pull files using general FromURLs method
+        fListDownloaded, fDictDownloaded = getFilesFromURLs(fileDictDownloads, dataName=dataName, dataPath=dataPath, ghRaw=ghRaw)
+        return fileDict, {'fileDictGithub':fileDict, 'fDictDownloaded':fDictDownloaded, 'fListDownloaded':fListDownloaded}
+
+    else:
+        return fileDict, fileDictDownloads

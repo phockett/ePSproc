c642abdb85e995880c8da1675783532e43ca7fc0
phockett <phockett@users.noreply.github.com>
2024-05-27 12:05:00 -0400

    Added autoapi for RTD builds.

--- docs/doc-source/conf.py
+++ docs/doc-source/conf.py
@@ -63,6 +63,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
                 'sphinxcontrib.apidoc', # 'recommonmark',
                 'sphinx.ext.viewcode', 'nbsphinx',
                 # 'myst_parser',
+                'autoapi.extension',   # 27/05/24 testing autoapi
                 'sphinx_mdinclude',    # 09/11/22 - testing MD include support, e.g. .. mdinclude:: ../../../docker/readme.md
                 # 'IPython.sphinxext.ipython_console_highlighting']  # Actually this throws an error on RTD - try adding ipyhton to requirements.txt instead...
                 'sphinx_math_dollar', 'sphinx.ext.mathjax'] # 22/05/24 For $ maths in RST, from https://github.com/sympy/sphinx-math-dollar
@@ -93,6 +94,9 @@ apidoc_module_dir = '../../epsproc'
 apidoc_output_dir = 'modules'
 apidoc_excluded_paths = ['tests']
 apidoc_separate_modules = True
+
+# 27/05/24 testing autoapi, https://github.com/readthedocs/sphinx-autoapi
+autoapi_dirs = '../../epsproc'
 
 # Sphinx-autodoc mock imports for minimal build-chain.
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_mock_imports
